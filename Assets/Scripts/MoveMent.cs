7675351e3b116d03e4d70cd291b28ea1d892487c
Harvey <zzsoicongtuzz@gmail.com>
2022-08-22 05:01:53 +1000

    Level 1 Greybox

---
+++ Assets/Scripts/MoveMent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMent : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    public Animator anim;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+       
+    }
+}
